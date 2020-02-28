903e137d544dfd9e460cbf19429e9eefd818aae4
Jai Singhal <jaisinghal48@gmail.com>
2020-02-29 05:04:42 +0530

    added comments

--- problem1/crack_classical.py
+++ problem1/crack_classical.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# crack.py
+# crack_classical.py
 
 __author__ = "Jai Singhal"
 __copyright__ = "Copyright Feb, 2020"
@@ -31,10 +31,7 @@ We can use space attack, just as we used in other question.
 import sys
 from BitVector import BitVector
 import string
-from collections import Counter
 
-
-BLOCK_THRESHOLD = 30
 
 # can be changed to anything, only first block get affected
 PassPhrase = "I want to learn cryptograph and network security"
@@ -43,7 +40,7 @@ PassPhrase = "I want to learn cryptograph and network security"
 Method 1:
 Space attack in MTP: If cipher text length is more than 180 characters, go for this method. 
 """
-class CrackCBCBySpaceAttack(object):
+class crackCBC(object):
     """
     CLass initializer
     """
@@ -67,38 +64,32 @@ class CrackCBCBySpaceAttack(object):
         return pphrase
 
     """
-    Returns bitvector of cipher read by file
+    Check for space, i.e, 0
     """
-    def readCipher(self, filename = "out.txt"):
-        ENCRYPTED_HEX = open(filename, 'r').readlines()[0]
-        cipher = BitVector(hexstring = ENCRYPTED_HEX)
-        return cipher
-
     @staticmethod
     def is_space(ch):
         # returns true if ord(char) is 0
         return True if ord(ch) == 0x00 else False
     
+    
+    """
+    Check for char is ascii character
+    """
     @staticmethod
     def is_ascii(ch):
         # returns true if ord(char) is in ascii
         return True if ch in string.ascii_letters else False
     
+    
+    """
+    Xor the two bitvectors
+    """
     @staticmethod
     def xor(s1, s2):
         # return xor of two bitvectors
         min_bvlen = min(len(s1), len(s2))
         return s1[:min_bvlen] ^ s2[:min_bvlen]
 
-
-    def spaces_count(self, text):
-        # count number of spaces in string
-        counter = Counter()
-        for index, char in enumerate(text):
-            # if space or any printable char
-            if self.is_space(char) or self.is_ascii(char):
-                counter[index] += 1
-        return counter
 
     """
     Removes the previous cipher blocks from each block
@@ -112,7 +103,9 @@ class CrackCBCBySpaceAttack(object):
             previous_cb = current_cb
         return newCipher
 
-
+    """
+    takes int key, and returns the bit vector object
+    """
     @staticmethod
     def generateKeyVector(key):
         # Returns the Bitvector of list of decimal values
@@ -133,63 +126,10 @@ class CrackCBCBySpaceAttack(object):
 
 
     """
-    Main FUnction that takes file name of cipher text,
-    and return the plain text.
-    Computes possible spaces in the text.
+    Get the last block by xoring with its previous one
+    Assuming last block is zero padded i.e., plain text
+    find the key, with this assumption.
     """
-    def attack(self, cfname):
-        cipher = self.readCipher(cfname)
-        totalCipherBlock = len(cipher) // self.BLOCKSIZE 
-        newCipher = self.removePrevCipherBlocks(cipher, totalCipherBlock)
-        
-        key = [None for _ in range(self.numbytes)]
-        
-        for i1 in range(0, totalCipherBlock):
-            counter = Counter()
-            c1 = newCipher[i1*self.BLOCKSIZE: (i1+1)*self.BLOCKSIZE]
-            for i2 in range(0, totalCipherBlock):
-                c2 = newCipher[i2*self.BLOCKSIZE: (i2+1)*self.BLOCKSIZE]
-                if i1 != i2:
-                    xored = self.xor(c1, c2)
-                    counter.update(
-                        self.spaces_count(xored.get_bitvector_in_ascii())
-                    )
-                    
-            for index, count in counter.items():
-                if count > totalCipherBlock*self.THRESHOLD:
-                    # pi ^ ci = key; pi = space
-                    outer_c = c1.get_bitvector_in_ascii()
-                    key[index] = self.SPACE_ASCII ^ ord(outer_c[index])
-
-        keybv = self.generateKeyVector(key)
-        return self.decryptMsg(newCipher, keybv, totalCipherBlock)
-
-
-
-"""
-Method 2: 
-Oracle Padding Attack: If cipher text length is less than 180 characters, go for this method. 
-"""
-class CrackCBCOracleAttack(object):
-    def __init__(self):
-        self.BLOCKSIZE = 64
-        self.numbytes = self.BLOCKSIZE // 8
-    
-    def getPassPhrase(self):
-        PassPhrase = "I want to learn cryptograph and network security"
-        # Reduce the passphrase to a bit array of size BLOCKSIZE:
-        pphrase = BitVector(bitlist = [0]*self.BLOCKSIZE)
-        for i in range(0,len(PassPhrase) // self.numbytes):
-            textstr = PassPhrase[i*self.numbytes:(i+1)*self.numbytes]
-            pphrase ^= BitVector( textstring = textstr )
-
-        return pphrase
-
-    def readCipher(self, filename = "ciphertext.txt"):
-        ENCRYPTED_HEX = open(filename, 'r').readlines()[0]
-        cipher = BitVector(hexstring = ENCRYPTED_HEX)
-        return cipher
-
     def getKeybyPadding(self, cipher):
         ### Getting KEy from last block
         clen = len(cipher)
@@ -204,13 +144,28 @@ class CrackCBCOracleAttack(object):
         return key
 
 
-    def attack(self, fname):
-        cipher = self.readCipher(fname)
-        pphrase = self.getPassPhrase()
-        key = self.getKeybyPadding(cipher)
+    """
+    Returns bitvector of cipher read by file
+    """
+    def readCipher(self, filename = "ciphertext.txt"):
+        ENCRYPTED_HEX = open(filename, 'r').readlines()[0]
+        cipher = BitVector(hexstring = ENCRYPTED_HEX)
+        return cipher
 
+
+    """
+    Other Main Function that takes cipher text and returns key
+    Computes key, if there is zero padding available
+    """
+    def orcale_paddng_attack(self, cipher):
         oldTotalChars = 0
-        # key = BitVector(size = 8, intVal = 0) + keyx[8:]
+        # get passphrase bv
+        pphrase = self.getPassPhrase()
+
+        # get key from oracle padding attack
+        key = self.getKeybyPadding(cipher)
+        # assume 7  bytes of key and initial 0 byte
+        key = BitVector(size = 8, intVal = 0) + key[8:]
         msg_encrypted_bv = BitVector( size = 0 )
         previous_block = pphrase
         for i in range(0, len(cipher) // self.BLOCKSIZE):
@@ -220,70 +175,70 @@ class CrackCBCOracleAttack(object):
             previous_block = cipher[i*self.BLOCKSIZE: (i+1)*self.BLOCKSIZE]
             msg_encrypted_bv += ct
 
-        return self.print_pt(msg_encrypted_bv, len(cipher))
+        return key
 
 
-    def print_pt(self, msg_encrypted_bv, cipher_len):
-        obtain_pt = BitVector( size = 0 )
-        NUMB_BLOCKS = cipher_len//self.numbytes//self.BLOCKSIZE
-        spacesUpper = list()
-        totalCharsFound = 0
-        plainText = str()
-        for i in range(0, cipher_len // 8):
-            char = msg_encrypted_bv[i*8: (i+1)*8].getTextFromBitVector()
-            obtain_pt += BitVector(textstring = char)
-            if char in string.ascii_uppercase:
-                spacesUpper.append(char)
-            if char in string.printable and char in string.ascii_letters:
-                plainText += char
-                totalCharsFound += 1
-            else:
-                plainText += "_"
-        return plainText
+    """
+    Main Function that takes file name of cipher text and key from orcale_paddng_attack,
+    and return the plain text.
+    Computes possible spaces in the text.
+    """
+    def spacing_mtp_attack(self, cipher, key):
+        totalCipherBlock = len(cipher) // self.BLOCKSIZE 
+        newCipher = self.removePrevCipherBlocks(cipher, totalCipherBlock)
+        
+        key = [ord(k) for k in key.get_bitvector_in_ascii()]
+        
+        for i1 in range(0, totalCipherBlock):
+            spaceCounter = dict()
+            c1 = newCipher[i1*self.BLOCKSIZE: (i1+1)*self.BLOCKSIZE]
+            for i2 in range(0, totalCipherBlock):
+                c2 = newCipher[i2*self.BLOCKSIZE: (i2+1)*self.BLOCKSIZE]
+                if i1 != i2:
+                    xored = self.xor(c1, c2)
+
+                    for index, char in enumerate(xored.get_bitvector_in_ascii()):
+                        # if space or any printable char
+                        if self.is_space(char) or self.is_ascii(char):
+                            if index not in spaceCounter.keys():
+                                spaceCounter[index] = 1
+                            else:
+                                spaceCounter[index] += 1
 
 
+            for index, count in spaceCounter.items():
+                if count > totalCipherBlock*self.THRESHOLD:
+                    # pi ^ ci = key; pi = space
+                    outer_c = c1.get_bitvector_in_ascii()
+                    key[index] = self.SPACE_ASCII ^ ord(outer_c[index])
+
+        keybv = self.generateKeyVector(key)
+        return self.decryptMsg(newCipher, keybv, totalCipherBlock)
+
+    """
+     Writes back the messages decrypted.
+    """
+    def writeDecryptedPlainText(self, decrypted_msg):
+        with open("recoveredtext.txt", "w", newline = "") as wb:
+            wb.write(decrypted_msg)
 
 
 if __name__ == "__main__":
-    # oracle_padding_attack()
     cfname = "ciphertext.txt"
     if sys.version_info[0] == 3:   
         cfname = input("\nEnter filname of cipher text to crack: ")
     else:                                                                         
         cfname = raw_input("\nEnter filname of cipher text to crack: ")
     
-
-
-    BLOCKSIZE = 64
-    NUMB_BLOCKS = 0
-    with open(cfname, "r") as cfc:
-        NUMB_BLOCKS = len(cfc.read())//(BLOCKSIZE//4)
-
-    print(f"Number of cipher blocks is: {NUMB_BLOCKS} blocks.")
-   
-    if NUMB_BLOCKS < BLOCK_THRESHOLD:
-        print("\nLength of cipher is small, so using Oracle Padding attack")
-        c = CrackCBCOracleAttack()
-        decrypted_msg = c.attack(cfname)
-    else:
-        print("Length of cipher is more than sufficient, so using Spacing MTP attack")
-        c = CrackCBCBySpaceAttack()
-        decrypted_msg = c.attack(cfname)
-
-
     print("Crack started!!")
     print("Please wait for crack to finish, this will take few seconds...")
 
+    c = crackCBC()
+    cipher = c.readCipher(cfname)
+    key = c.orcale_paddng_attack(cipher)
+    decrypted_msg = c.spacing_mtp_attack(cipher, key).strip("\n \0\t")
+    c.writeDecryptedPlainText(decrypted_msg)
 
-    dmsg = str()
-    for ch in decrypted_msg:
-        if ch in string.ascii_letters or ch in string.printable:
-            dmsg += ch
-        else:
-            dmsg += "_"
-
-    with open("recoveredtext.txt", "w") as wb:
-        wb.write(dmsg)
-    print(f"\n\nPlain text is written into recoveredtext.txt file.")
+    print(f"\n\nPlain text is written into recoveredtext.txt file.\n")
     
  
--- problem2/crack_mtp.py
+++ problem2/crack_mtp.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python
+# crack_mtp.py
+
+__author__ = "Jai Singhal"
+__copyright__ = "Copyright Feb, 2020"
+__version__ = "1.0.0"
+__maintainer__ = "Jai Singhal"
+__email__ = "h20190021@pilani.bits-pilani.ac.in"
+__website__ = "http://jai-singhal.github.io"
+__github_repo__ = "https://github.com/jai-singhal/encrypt-decrypt"
+
 """
 Space := ord(" ") # utf-8 or ASCII val of space: 32
 
@@ -9,17 +20,13 @@ Space XOR Space = 0
 X XOR Space = x; where X = [A-Z], x = [a-z] and vice versa
 ord("A")^ord(" ") = ord("A")
 **
-
 k ^ pi = ci
-
 """
 # TODO: change the CT
 
-import collections
 import string
 from BitVector import BitVector
 import sys
-import re
 import os
 from pprint import pprint
 
@@ -30,23 +37,36 @@ class CrackMTP(object):
         self.key = list()
         self.threshold = 0.85
 
-    def is_space(self, ch):
+    """
+    Check for space, i.e, 0
+    """
+    @staticmethod
+    def is_space(ch):
+        # returns true if ord(char) is 0
         return True if ord(ch) == 0x00 else False
 
-    def is_ascii(self, ch):
+    """
+    Check for char is ascii character
+    """
+    @staticmethod
+    def is_ascii(ch):
+        # returns true if ord(char) is in ascii
         return True if ch in string.ascii_letters else False
     
-    def xor(self, s1, s2):
+
+    """
+    Xor the two bitvectors
+    """
+    @staticmethod
+    def xor(s1, s2):
+        # return xor of two bitvectors
         min_bvlen = min(len(s1), len(s2))
         return s1[:min_bvlen] ^ s2[:min_bvlen]
 
-    def count_spaces(self, txt):
-        counter = collections.Counter()
-        for index, char in enumerate(txt):
-            if self.is_space(char) or self.is_ascii(char):
-                counter[index] += 1
-        return counter
 
+    """
+    Returns bitvector of cipher read by file
+    """
     def read_ciphers(self, fname):
         ciphertexts = list()
         with open(fname, "r") as ctf:
@@ -55,26 +75,28 @@ class CrackMTP(object):
                 ciphertexts.append(BitVector(hexstring = ct))
         return ciphertexts
 
-    def spaces_count(self, text):
-        counter = collections.Counter()
-        for index, char in enumerate(text):
-            # if space or any printable char
-            if self.is_space(char) or self.is_ascii(char):
-                counter[index] += 1
-        return counter
-
+    """
+    Main Function that takes file name of cipher text and key from orcale_paddng_attack,
+    and return the plain text.
+    Computes possible spaces in the text.
+    """
     def crack_hrd(self, ciphers):
         key = [None for _ in range(len(ciphers[0])//8)]
-        for i1, c1 in enumerate(ciphers):
-            counter = collections.Counter()
-            for i2, c2 in enumerate(ciphers):
-                if i1 != i2:
+        for c1 in ciphers:
+            spaceCounter = dict()
+            for c2 in ciphers:
+                if c1 != c2:
                     xored = self.xor(c1, c2)
-                    counter.update(
-                        self.spaces_count(xored.get_bitvector_in_ascii())
-                    )
-                    
-            for index, count in counter.items():
+                    for index, char in enumerate(xored.get_bitvector_in_ascii()):
+                        # if space or any printable char
+                        if self.is_space(char) or self.is_ascii(char):
+                            if index not in spaceCounter.keys():
+                                spaceCounter[index] = 1
+                            else:
+                                spaceCounter[index] += 1
+
+
+            for index, count in spaceCounter.items():
                 # if count == len(ciphers) - 1:
                 if count > len(ciphers)*self.threshold:
                     # pi ^ ci = key; pi = space
@@ -82,7 +104,11 @@ class CrackMTP(object):
                     key[index] = self.SPACE_ASCII ^ ord(outer_c[index])
         return key
 
-    def crack(self, ctfile):
+    """
+    Main function, which calls crack_hrd for cipher text,
+    reduces the min length cipher, to get the plain text from big ciphers.
+    """
+    def spacing_mtp_attack(self, ctfile):
         
         self.ciphertexts = self.read_ciphers(ctfile)
         ciphers = sorted(self.ciphertexts, key = len)
@@ -99,10 +125,18 @@ class CrackMTP(object):
                 if j is not None:
                     pt += chr(ord(i) ^ j)
                 else:
-                    pt += "_"
+                    pt += "*"
             plaintexts.append(pt)
         return plaintexts
                 
+
+    """
+     Writes back the messages decrypted.
+    """
+    def writeDecryptedPlainText(self, decrypted_msg):
+        with open("recoveredtext.txt", "w", newline = "") as wb:
+            wb.write("\n".join(decrypted_msg))
+
 
 if __name__ == "__main__":
     cfname = "ciphertext.txt"
@@ -113,9 +147,9 @@ if __name__ == "__main__":
 
     print("Crack started!!")
     print("Please wait for crack to finish, this will take few seconds...")
-    c = CrackMTP()
-    decrypted_msg = c.crack(cfname)
 
-    with open("recoveredtext.txt", "w") as wb:
-        wb.write("\n".join(decrypted_msg))
-    print(f"\n\nPlain text is written into recoveredtext.txt file.")
+    c = CrackMTP()
+    decrypted_msg = c.spacing_mtp_attack(cfname)
+    c.writeDecryptedPlainText(decrypted_msg)
+        
+    print(f"\nPlain text is written into recoveredtext.txt file.\n\n")
